af949532cc096574606d507a70a1352633639f43
Theo Wyzgowski <theowyz@btinternet.com>
2021-07-30 04:13:58 +0100

    portfolio clarity fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="">
-      <header>
+      <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.12.0/devicon.min.css" />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
-      </header>
+        <script src="https://kit.fontawesome.com/4fb58de0f3.js" crossorigin="anonymous"></script>
+      </head>
 
 
       {/* TOP SECTION*/}
@@ -42,25 +43,25 @@ function App() {
 
         <section className=" section flexbox-container has-background-black">
           <div className="flexbox-item flexbox-item-1 devicons has-text-white has-background-black">
-            <div class="devicon-div"><i class="devicon-html5-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-css3-plain"></i></div>
+            <div class="devicon-div"><i class="devicon-html5-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-css3-plain-wordmark"></i></div>
             <div class="devicon-div"><i class="devicon-javascript-plain"></i></div>
             <div class="devicon-div"><i class="devicon-bulma-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-git-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-nodejs-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-react-original"></i></div>
-            <div class="devicon-div"><i class="devicon-github-original"></i></div>
-            <div class="devicon-div"><i class="devicon-python-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-mongodb-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-express-original"></i></div>
+            <div class="devicon-div"><i class="devicon-git-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-nodejs-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-react-original-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-github-original-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-python-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-mongodb-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-express-original-wordmark"></i></div>
             <div class="devicon-div"><i class="devicon-django-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-postgresql-plain"></i></div>
+            <div class="devicon-div"><i class="devicon-postgresql-plain-wordmark"></i></div>
             <div class="devicon-div"><i class="devicon-npm-original-wordmark"></i></div>
-            <div class="devicon-div"><i class="devicon-vscode-plain"></i></div>
+            <div class="devicon-div"><i class="devicon-vscode-plain-wordmark"></i></div>
             <div class="devicon-div"><i class="devicon-sass-original"></i></div>
-            <div class="devicon-div"><i class="devicon-jquery-plain"></i></div>
-            <div class="devicon-div"><i class="devicon-heroku-original"></i></div>
-            <div class="devicon-div"><i class="devicon-trello-plain"></i></div>
+            <div class="devicon-div"><i class="devicon-jquery-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-heroku-plain-wordmark"></i></div>
+            <div class="devicon-div"><i class="devicon-trello-plain-wordmark"></i></div>
           </div>
 
           <div className="flexbox-item flexbox-item-2 has-text-centered has-text-white has-background-black">
@@ -75,8 +76,8 @@ function App() {
         {/* PROJECTS */}
         <section className=" section has-background-white has-text-black">
           <h1 class="has-text-centered is-size-2">Projects</h1>
-          <br/>
-<h2 className="box has-text-centered">These projects were created during my time on the General Assembly Software Engineering Immersive course. Hover over each project to discover more about it, and click to visit!</h2>
+          <br />
+          <h2 className="box has-text-centered">These projects were created during my time on the General Assembly Software Engineering Immersive course. Hover over each project to discover more about it, and click to visit!</h2>
           <br />
           <div className="flexbox-container-projects">
 
@@ -86,7 +87,7 @@ function App() {
                 <div className="column">
                   <h3 className="card-header-title text is-centered is-size-4">Metroid Snake</h3>
                   <div className="image is-1by1 img__wrap grow">
-                    <img class="img__img project-images is-rounded" src="https://i.imgur.com/WlwChmr.png" alt="Click Here!" />
+                    <img class="img__img project-images" src="https://i.imgur.com/WlwChmr.png" alt="Click Here!" />
                     <div class="img__description_layer">
                       <p className="img__description has-text-centered">A Nintendo-themed variant of Snake. Defeat the Metroids, grow your army! Created in 1 week with HTML, CSS and JavaScript.</p>
                     </div>
@@ -94,9 +95,9 @@ function App() {
                 </div>
               </a>
               <a href="https://github.com/tdore92/project-1" alt="" target="_blank" rel="noopener noreferrer">
-              <div className="card-footer-item github-icon grow">
-                <i class="devicon-github-original"></i>
-              </div>
+                <div className="card-footer-item github-icon grow">
+                  <i class="devicon-github-original"></i>
+                </div>
               </a>
 
             </div>
@@ -107,7 +108,7 @@ function App() {
                   <div className="column">
                     <div className="card-header-title text is-centered is-size-4">BrewDog</div>
                     <div className="img__wrap image is-1by1 grow">
-                      <img class="img__img project-images is-rounded" src="https://i.imgur.com/XdQ5x0g.png" alt="Click Here!" />
+                      <img class="img__img project-images" src="https://i.imgur.com/XdQ5x0g.png" alt="Click Here!" />
                       <div class="img__description_layer">
                         <p className="img__description has-text-centered">Browse BrewDogs wares on this React App, created in 48 hours with a third party API and Axios requests.</p>
                       </div>
@@ -117,9 +118,9 @@ function App() {
                 </div>
               </a>
               <a href="https://github.com/tdore92/sei-project-2" alt="" target="_blank" rel="noopener noreferrer">
-              <div className="card-footer-item github-icon grow">
-                <i class="devicon-github-original"></i>
-              </div>
+                <div className="card-footer-item github-icon grow">
+                  <i class="devicon-github-original"></i>
+                </div>
               </a>
             </div>
 
@@ -127,16 +128,16 @@ function App() {
               <div className="column">
                 <h3 className="card-header-title text is-centered is-size-4">MontVenture</h3>
                 <div className="img__wrap image is-1by1 grow">
-                  <img class="img__img project-images is-rounded" src="https://i.imgur.com/RAvdPV4.png?1" alt="Click Here!" />
+                  <img class="img__img project-images" src="https://i.imgur.com/RAvdPV4.png?1" alt="Click Here!" />
                   <div class="img__description_layer">
                     <p className="img__description has-text-centered">Search for UK based mountaineering activities with our MontVenture app. Created in 1 week with node.js, React.js and mongoDB.</p>
                   </div>
                 </div>
               </div>
               <a href="https://github.com/tdore92/project-3-server" alt="" target="_blank" rel="noopener noreferrer">
-              <div className="card-footer-item github-icon grow">
-                <i class="devicon-github-original"></i>
-              </div>
+                <div className="card-footer-item github-icon grow">
+                  <i class="devicon-github-original"></i>
+                </div>
               </a>
             </div>
 
@@ -144,16 +145,16 @@ function App() {
               <div className="column">
                 <h3 className="card-header-title text is-centered is-size-4">Dinosaur Petshop</h3>
                 <div className="image is-1by1 img__wrap grow">
-                  <img class="img__img project-images is-rounded" src="https://i.imgur.com/I22et98.png" alt="Click Here!" />
+                  <img class="img__img project-images" src="https://i.imgur.com/I22et98.png" alt="Click Here!" />
                   <div class="img__description_layer">
-                    <p className="img__description has-text-centered">Dinosaur Petshop</p>
+                    <p className="img__description has-text-centered">An online pet shop selling dinosaurs and assorted items- take the Q&amp;A to find the right dinosaur for you. Uses a Python Django backend and a React front end.</p>
                   </div>
                 </div>
               </div>
               <a href="https://github.com/tdore92/project-4-server" alt="" target="_blank" rel="noopener noreferrer">
-              <div className="card-footer-item github-icon grow">
-                <i class="devicon-github-original"></i>
-              </div>
+                <div className="card-footer-item github-icon grow">
+                  <i class="devicon-github-original"></i>
+                </div>
               </a>
             </div>
 
@@ -167,17 +168,22 @@ function App() {
         <section className="section has-background-black has-text-white">
           <h1 className="has-text-centered is-size-2">My Interests</h1>
           <br />
-          <div className="columns">
-            <div className="column is-one-fifth has-text-centered">
+          <div className="columns flexbox-container section">
+            <div className="column is-one-third has-text-centered">
               <a href="https://www.youtube.com/channel/UCaCnFiV6EODc_d2_vO9rDGw" alt="YT Link" target="_blank" rel="noopener noreferrer">
-              <h2>Film &amp; Animation</h2>
+                <img class="interests-icon grow" src="http://pngmesh.com/media/1616166067.png" alt="YT Icon" />
+              </a>
+              <h2 className="is-size-5">Film &amp; Animation</h2>
               <hr />
               <p>I love to create animated works using Unreal Engine, Source Filmmaker and various other programs. Click the icon to find out more!</p>
-              </a>
+
             </div>
-            <div className="column is-one-fifth has-text-centered interests-box">
-              <h2>Software Development</h2>
-              <hr/>
+            <div className="column is-one-third has-text-centered interests-box">
+              <a href="https://github.com/tdore92" alt="Blog Link" target="_blank" rel="noopener noreferrer">
+                <img class="interests-icon grow" src="https://icons-for-free.com/iconfiles/png/512/blog+blogging+compose+resolutions+sign+write+icon-1320084094980840700.png" alt="Blog Icon" />
+              </a>
+              <h2 className="is-size-5">Software Development</h2>
+              <hr />
               <p>I'm currently developing YouWatch- a blog based around showing and and expressing your internet viewing habits. Click the icon to follow its progress!</p>
             </div>
           </div>
@@ -185,12 +191,13 @@ function App() {
         </section>
 
         {/* CONTACT DETAILS */}
-<section className="section">
-        <div className="has-text-centered">
-          <h2>Contact</h2>
-          <p>theowyz@btinternet.com</p>
-        </div>
-          </section>
+        <section className="section">
+          <div className="has-text-centered">
+            <h2 className="is-size-2">Contact</h2>
+            <hr/>
+            <p className="is-size-3">theowyz@btinternet.com</p>
+          </div>
+        </section>
       </body>
     </div>
   )
