ed4afe762288f11b2f521c4abd9ab42ffc9e1138
Theo Wyzgowski <theowyz@btinternet.com>
2021-09-14 22:07:06 +0100

    blog deco 1

--- src/components/common/Blog.js
+++ src/components/common/Blog.js
@@ -2,7 +2,23 @@ function Blog() {
 
   return (
     <>
-    <h1>This is the blog page</h1>
+    <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Potta+One&display=swap" rel="stylesheet" />
+    </head>
+      <header className="hero hero-card">
+        <div className="hero-body container has-text-centered has-text-white">
+          <h1 className="is-size-1 section">THE BOOK OF THEODORE</h1>
+          <p>Welcome to my blog! This will mainly detail my journey developing my different projects in Javascript, React, Python and Node.js, at least until I get a full-time job. Just kidding. But not really. I mean, maybe.</p>         
+        </div>
+      </header>
+
+      <section className="section">
+        <div className="is-size-4">Blogs on the way</div>
+        <div className="is-size-5">I promise</div>
+        <p>(This is the basic layout)</p>
+      </section>
     </>
   )
 
